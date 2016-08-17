b657f0db631d09b536622b2ec3c42b3889694a41
Deft_MKJing <mkjing@mkjingdemac-mini.local>
2016-08-17 11:00:34 +0800

    Initial Commit

---
+++ MKJWechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKJWechat
+//
+//  Created by MKJING on 16/8/17.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
